bfabedad7392af25076fd8f2ed586776dff5757c
priya-vibhute <priyanka.vibhute@itvedant.com>
2025-09-27 19:28:10 +0530

    27 sept

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Formhandling1 } from "./components/formhandling1/formhandling1";
+import { Formhandling2 } from "./components/formhandling2/formhandling2";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Formhandling1],
+  imports: [RouterOutlet, Formhandling1, Formhandling2],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/formhandling2/formhandling2.ts
+++ src/app/components/formhandling2/formhandling2.ts
@@ -1,11 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+
+export function commonPasswordValidator(control:AbstractControl):ValidationErrors|null
+{
+  let commonPasswords=["abcd","1234","password"]
+
+  if(control.value && commonPasswords.includes(control.value))
+  {
+      return {'commonPassword':true}
+  }
+  else
+  {
+    return null
+  }
+
+}
+
+
+
 
 @Component({
   selector: 'app-formhandling2',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './formhandling2.html',
   styleUrl: './formhandling2.css'
 })
 export class Formhandling2 {
+
+   userForm!:FormGroup;
+
+   constructor(private formbuilder :FormBuilder)
+   {
+     this.userForm=this.formbuilder.group({
+      name:['nisha',[Validators.required,Validators.minLength(3)]],
+      email:['a@gmail.com',[Validators.required,Validators.email]],
+      password:['',[Validators.required,commonPasswordValidator]]
+     })
+   }
+
+   onSubmit()
+   {
+     console.log(this.userForm); 
+   }
 
 }
